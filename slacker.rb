9f003b4e079f07383b31d0ceb85f9d8396689f71
Cj <cj-bc@y-modify.org>
2018-02-11 18:28:29 +0900

    Add: formulae itself
    this formulae is version 0.1.3

---
+++ slacker.rb
@@ -0,0 +1,22 @@
+class Slacker < Formula
+  version = "0.1.3"
+  desc "Allows to send slack messages from terminal"
+  homepage "https://github.com/Cj-bc/slacker"
+  url "https://github.com/Cj-bc/slacker/archive/v" + version + ".tar.gz"
+  sha256 "96f8d04a1c7b6e2b08df6ecd862a1bf699219d238b5fdbca748d47c4bef88e6c"
+  head "https://github.com/Cj-bc/slacker.git"
+
+  depends_on "awk"
+  depends_on "python"
+
+ def install 
+	  ohai "Renaming executable..."
+	  system "mv", "slacker.sh", "slacker" # Rename
+	  bin.install "slacker" # Install slacker
+	  prefix.install Dir["func"], Dir["texts"], ".sourcefiles" # Install all texts and functions
+ end
+
+  test do
+    system "slacker"
+  end
+end
